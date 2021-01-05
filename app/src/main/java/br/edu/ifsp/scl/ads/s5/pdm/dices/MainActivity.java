580f7a1a0e9b95fe15a3cf1d76f32e71ed9e7250
Rogerio Colaço <rcs@ufscar.br>
2021-01-05 20:12:42 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/s5/pdm/dices/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.ads.s5.pdm.dices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
