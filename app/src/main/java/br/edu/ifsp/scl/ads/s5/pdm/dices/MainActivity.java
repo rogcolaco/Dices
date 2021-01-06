96baf955c2caeca8cea4bc2ab90bc401e9297341
Rogerio Colaço <rcs@ufscar.br>
2021-01-05 21:22:52 -0300

    - Inclusão das imagens; - Configuração imagem laucher; - Layout da activity_main.xml;

--- app/src/main/java/br/edu/ifsp/scl/ads/s5/pdm/dices/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/s5/pdm/dices/MainActivity.java
@@ -3,12 +3,25 @@ package br.edu.ifsp.scl.ads.s5.pdm.dices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import br.edu.ifsp.scl.ads.s5.pdm.dices.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+    private TextView mensagemTv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v(getString(R.string.app_name), "onCreate executado - iniciado ciclo de vida completo");
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
     }
 }
