db81133124c47c5be4b6875ad22b437e493491a3
Rogerio Colaço <rcs@ufscar.br>
2021-01-10 11:32:00 -0300

    - layout configuracoes inclusão de campos e botões;

--- app/src/main/java/br/edu/ifsp/scl/ads/s5/pdm/dices/ConfiguracoesActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/ads/s5/pdm/dices/ConfiguracoesActivity.java
@@ -1,6 +1,9 @@
 package br.edu.ifsp.scl.ads.s5.pdm.dices;
 
 import android.app.Activity;
+import android.view.View;
 
 public class ConfiguracoesActivity extends Activity {
+    public void onClick(View view) {
+    }
 }
